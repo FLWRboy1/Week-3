c2a82d8c7a18e3090b299fb5fde0064bebedb347
FLWRboy1 <frankmullahy@gmail.com>
2024-09-24 10:02:10 -0400

    Update FirstFile.py

--- FirstFile.py
+++ FirstFile.py
@@ -1,3 +1,6 @@
-sigma = 'Patrick'
-alpha = 'Bateman'
-print(sigma,alpha)
+firstName = 'Patrick'
+lastName = 'Bateman'
+age = '4'
+
+print(firstName,lastName,str(age),sep=',')
+print(firstName+","+lastName+","+str(age),sep=',')
